7de33e3255110a3c55e9f316d19d31cc5ff17c23
Vinayrocks5775 <132809422+Vinayrocks5775@users.noreply.github.com>
2025-07-30 17:28:29 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-streamlit run app.py --server.port 8000 --server.enableCORS false
+echo "Launching Streamlit app on Azure..."
+streamlit run app.py --server.enableCORS false --server.port 8000 --server.address 0.0.0.0
+
