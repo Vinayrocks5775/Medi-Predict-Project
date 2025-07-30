a5818c086535193f12217c4c7d318952769d32cf
Vinayrocks5775 <132809422+Vinayrocks5775@users.noreply.github.com>
2025-07-30 18:18:57 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-echo "Launching Streamlit app on Azure..."
-streamlit run app.py --server.enableCORS false --server.port 8000 --server.address 0.0.0.0
+echo "Creating venv and installing dependencies..."
+python3.10 -m venv antenv
+source antenv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+
+echo "Launching app..."
+streamlit run app.py --server.port 8000 --server.address 0.0.0.0
+
