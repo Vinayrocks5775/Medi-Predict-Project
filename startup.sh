22f47cad4f77fd4502936c341b533ab3b1eb7038
Vinayrocks5775 <132809422+Vinayrocks5775@users.noreply.github.com>
2025-07-30 18:41:25 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-echo "Creating venv and installing dependencies..."
 python3.10 -m venv antenv
 source antenv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
-
-echo "Launching app..."
 streamlit run app.py --server.port 8000 --server.address 0.0.0.0
+
